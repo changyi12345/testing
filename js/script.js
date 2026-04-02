bd9d6f35b054389ce8e5c951a00712d2de537dc2
changyi12345 <chanpyae7722@gmail.com>
2026-04-02 12:07:41 +0630

    Add project files

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.menu').style.display = "flex"
+    document.querySelector('.menu').style.left = 0
+})
